8ec282a97f6069569189c47c87264c07e9735a32
Ben Deltenre <benjdelt@gmail.com>
2020-02-18 18:50:53 -0800

    Created models wip

--- backend/daves_netflix/models.py
+++ backend/daves_netflix/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=100, default="", null=False)
+    handle = models.CharField(max_length=30)
+    avatar_url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class StreamingService(models.Model):
+    name = models.CharField(max_length=30)
+    logo_url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class StreamingAccount(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    streaming_service = models.ForeignKey(StreamingService, on_delete=models.CASCADE)
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    spots = models.IntegerField()
+    price = models.IntegerField()
+    four_k = models.BooleanField(default=False)
+    users = models.ManyToManyField(User, through="UserStreamingAccount")
+
+    def __str_(self):
+        return self.name
+
+class UserStreamingAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    streaming_account = models.ForeignKey(StreamingAccount, on_delete=models.CASCADE)
+
+    def __str_(self):
+        return self.name
