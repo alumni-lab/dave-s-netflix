1408a8bd9bfc18c9924f91fba293ab04008d07d8
Ben Deltenre <benjdelt@gmail.com>
2020-02-25 18:14:20 -0800

    Registered models in admin site

--- backend/daves_netflix/admin.py
+++ backend/daves_netflix/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(StreamingService)
+admin.site.register(StreamingAccount)
+admin.site.register(UserStreamingAccount)
